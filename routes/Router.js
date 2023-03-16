667f2077c1d6708deb8c793b5a619a08807ef182
victorbuquera <victorbuquera@gmail.com>
2023-03-15 22:31:12 -0300

    login/manter logado ok

--- routes/Router.js
+++ routes/Router.js
@@ -8,7 +8,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Router() {
   const dispatch = useDispatch();
-  const loggedIn = useSelector((state) => state.auth.loggedIn);
+  const loggedIn = useSelector((state) => state.auth.isAuthenticated);
 
   useEffect(() => {
     const checkLoggedIn = async () => {
